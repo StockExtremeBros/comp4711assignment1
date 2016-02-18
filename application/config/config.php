31d0524960d6df2e83792223a926c937493150a0
Gabriella Cheung <gabriella.bcit@gmail.com>
2016-02-18 12:21:50 -0800

    New page to show player portfolio, drop down populated with players from database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://assign1.local:4711/';
+$config['base_url'] = '/';
 
 /*
 |--------------------------------------------------------------------------
