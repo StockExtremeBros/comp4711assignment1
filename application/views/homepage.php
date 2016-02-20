6934fdd845beb63d30a8d20fa363e8784892fb3b
Tyler Trepanier <tmanthegamer@gmail.com>
2016-02-19 16:00:27 -0800

    Styling tables.

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -4,11 +4,13 @@
     </h1>
     <div class="row">
         <div class="col-lg-6">
+            <h3>Stock Overview</h3>
             <div class="table-responsive">
                 {stockoverview}
             </div>
         </div>
         <div class="col-lg-6">
+            <h3>Player Overview</h3>
             <div class="table-responsive">
                 {playeroverview}
             </div>
