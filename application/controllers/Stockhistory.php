ea1489675f136cb6254a1f7fc3847a985316dc2b
Gabriella Cheung <gabriella.bcit@gmail.com>
2016-04-05 17:48:38 -0700

    Revert Stockhistory class name

--- application/controllers/Stockhistory.php
+++ application/controllers/Stockhistory.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Stockhistory extends Application {
+class StockHistory extends Application {
 
 	/**
 	 * Index Page for this controller.
