f78333b2ba59ce8128fa16aed6ee4a0a6550bb57
Gabriella Cheung <gabriella.bcit@gmail.com>
2016-04-05 17:21:28 -0700

    Fixed Stockhistory file and class name

--- application/controllers/Stockhistory.php
+++ application/controllers/Stockhistory.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class StockHistory extends Application {
+class Stockhistory extends Application {
 
 	/**
 	 * Index Page for this controller.
