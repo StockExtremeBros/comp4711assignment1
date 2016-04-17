93ad9dde7638963ffbddf419e43f769d3709d852
Tyler Trepanier <tmanthegamer@gmail.com>
2016-04-16 18:00:05 -0700

    base_url reset to default

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://comp4711.local:4706/';
+$config['base_url'] = '/';
 
 /*
 |--------------------------------------------------------------------------
